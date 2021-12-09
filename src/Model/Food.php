75c890bc60a10184af1ddfd9c5303c3082db6589
MOBISARE <m.luc57@outlook.fr>
2021-12-09 08:48:22 +0100

    Class Food.php faite

---
+++ src/Model/Food.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace src\Model\Entity;
+
+class Food{
+
+    private $id
+
+    private $name
+
+    public function __construct($id, $name){
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getId(){
+        return $this->$id;
+    }
+
+    public function getName(){
+        return $this->$name;
+    }
+}
+
