2e0e40f7a9d5e504659c062ccb0ab3325a82bc27
MOBISARE <m.luc57@outlook.fr>
2021-12-09 08:53:01 +0100

    Class Ingredient et Recipe faites

---
+++ src/Model/Ingredient.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace src\Model\Entity;
+
+class Ingredient{
+
+    private $id;
+
+    private $name;
+
+    public function __constr($id, $name){
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+}
